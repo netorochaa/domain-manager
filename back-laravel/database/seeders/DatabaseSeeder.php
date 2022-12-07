e4ff88a6ba699895eac874c1b7ece903d863548a
netorochaa <jrr.neto@hotmail.com>
2022-12-07 19:50:48 -0300

    create domain model

--- back-laravel/database/seeders/DatabaseSeeder.php
+++ back-laravel/database/seeders/DatabaseSeeder.php
@@ -3,22 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Domain;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Domain::factory(3)->create();
     }
 }
