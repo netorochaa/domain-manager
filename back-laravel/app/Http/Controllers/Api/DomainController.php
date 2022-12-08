2877fdc66ce18ffea846fb8797052cae0a1cc396
netorochaa <jrr.neto@hotmail.com>
2022-12-08 16:24:42 -0300

    Create Domain controller api

---
+++ back-laravel/app/Http/Controllers/Api/DomainController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Domain;
+use Illuminate\Http\Request;
+
+class DomainController extends Controller
+{
+    public function index()
+    {
+        return Domain::query()
+            ->select('id', 'name', 'tld')
+            ->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Domain  $domain
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Domain $domain)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Domain  $domain
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Domain $domain)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Domain  $domain
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Domain $domain)
+    {
+        //
+    }
+}
--- back-laravel/routes/api.php
+++ back-laravel/routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\DomainController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('domains', [DomainController::class, 'index'])->name('domain.index');
+Route::get('domains/create', [DomainController::class, 'create'])->name('domain.create');
+Route::post('domains', [DomainController::class, 'store'])->name('domain.store');
+Route::get('domains/{domain}/edit', [DomainController::class, 'edit'])->name('domain.edit');
+Route::put('domains/{domain}', [DomainController::class, 'update'])->name('domain.update');
+Route::delete('domains/{domain}', [DomainController::class, 'destroy'])->name('domain.destroy');
