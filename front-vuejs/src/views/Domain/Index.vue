c718d7f312e4854bf5b13a10c908f9f29ebd18eb
netorochaa <jrr.neto@hotmail.com>
2022-12-08 17:58:23 -0300

    Create index domains

---
+++ front-vuejs/src/views/Domain/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="d-flex flex-column">
+    <header class="title">
+      <h1>Domains</h1>
+    </header>
+    <table class="table table-striped table-responsive">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Tld</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="domain in domains" :key="domain.id">
+          <td>{{ domain.id }}</td>
+          <td>{{ domain.name }}</td>
+          <td>{{ domain.tld }}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        domains: []
+      }
+    },
+
+    created(){
+      fetch('http://localhost:8000/api/domains')
+        .then(response => response.json())
+        .then(records => this.domains = records)
+    }
+  }
+</script>
+
+<style>
+</style>
